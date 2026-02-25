8bb548ec2056f590f76abc0d7c4e8f40b0d02277
EmmettWalthers <itsemmettdesign@gmail.com>
2026-02-24 22:23:45 -0500

    Added random target movement
    Circle spawns randomly and when clicked adds 1 score and moves somewhere random. Bug right now, when it gets close to top you can't click it

--- projects/CART263-PROJECT-1/js/index.js
+++ projects/CART263-PROJECT-1/js/index.js
@@ -1,3 +1,30 @@
 "use strict";
 
 let score = 0;
+
+function moveTarget() {
+
+    const target = document.getElementById("target");
+
+    const x = Math.floor(Math.random() * (window.innerWidth - 50));
+    const y = Math.floor(Math.random() * (window.innerHeight - 50));
+
+    target.style.left = x + "px";
+    target.style.top = y + "px";
+
+    target.style.transform = 'none';
+
+    };
+
+target.addEventListener("click", function() {
+        
+    score = score + 1;
+
+    document.getElementById("score").textContent = score;
+
+    moveTarget();
+
+});
+
+moveTarget();
+
